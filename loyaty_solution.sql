0f24c22db62ab4be93b8768b641929058ace4ce0
Dayour5539 <59515978+Dayour5539@users.noreply.github.com>
2022-04-01 15:48:24 +0100

    Update loyaty_solution.sql

--- loyaty_solution.sql
+++ loyaty_solution.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2022 at 03:25 PM
+-- Generation Time: Apr 01, 2022 at 01:40 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.27
 
@@ -46,12 +46,12 @@ CREATE TABLE `citizens` (
 --
 
 INSERT INTO `citizens` (`id`, `name`, `gender`, `address`, `phone`, `ward_id`, `created_at`, `updated_at`, `lga_id`, `state_id`) VALUES
-(1, 'Bola Tinubu', 'Female', 'His House', 7088445522, 2, '2022-04-01 11:09:53', '2022-04-01 11:09:53', '1', '4'),
-(2, 'Vincent Kato', 'Male', 'Abjua, North Central', 8055226699, 2, '2022-04-01 11:11:18', '2022-04-01 11:11:18', '4', '5'),
-(3, 'Cecelia Chuwkma', 'Female', 'Kano', 8077552211, 4, '2022-04-01 11:12:09', '2022-04-01 11:12:09', '2', '5'),
-(4, 'John Doe', 'Male', 'Playlearn, Abeokuta', 8054759632, 3, '2022-04-01 11:18:02', '2022-04-01 11:18:02', '3', '2'),
-(5, 'Bola Adejobi', 'Male', 'Oko Town, Anambra', 8099547852, 1, '2022-04-01 11:19:28', '2022-04-01 11:19:28', '5', '1'),
-(6, 'Yinka Razaq', 'Male', 'Illorin', 9124563285, 2, '2022-04-01 11:20:59', '2022-04-01 11:20:59', '4', '6');
+(1, 'Bola Tinubu', 'Female', 'Her House', 7088445522, 2, '2022-04-01 14:14:53', '2022-04-01 14:14:53', '1', '4'),
+(2, 'Vincent Kato', 'Male', 'Abjua, North Central', 8055226699, 2, '2022-04-01 14:11:18', '2022-04-01 14:11:18', '4', '5'),
+(3, 'Cecelia Chuwkma', 'Female', 'Kano', 8077552211, 4, '2022-04-01 14:12:09', '2022-04-01 14:12:09', '2', '5'),
+(4, 'John Doe', 'Male', 'Playlearn, Abeokuta', 8054759632, 3, '2022-04-01 14:18:02', '2022-04-01 14:18:02', '3', '2'),
+(5, 'Bola Adejobi', 'Male', 'Oko Town, Anambra', 8099547852, 1, '2022-04-01 14:19:28', '2022-04-01 14:19:28', '5', '1'),
+(6, 'Yinka Razaq', 'Male', 'Illorin', 9124563285, 2, '2022-04-01 14:20:59', '2022-04-01 14:20:59', '4', '6');
 
 -- --------------------------------------------------------
 
@@ -72,11 +72,11 @@ CREATE TABLE `lgas` (
 --
 
 INSERT INTO `lgas` (`id`, `name`, `state_id`, `created_at`, `updated_at`) VALUES
-(1, 'Aba North', 1, '2022-04-01 10:53:40', '2022-04-01 10:53:40'),
-(2, 'Aba South', 2, '2022-04-01 10:53:58', '2022-04-01 10:53:58'),
-(3, 'Arochukwu', 3, '2022-04-01 10:54:26', '2022-04-01 10:54:26'),
-(4, 'Bende', 4, '2022-04-01 10:54:44', '2022-04-01 10:54:44'),
-(5, 'Ikwuano', 5, '2022-04-01 10:55:02', '2022-04-01 10:55:02');
+(1, 'Aba North', 1, '2022-04-01 14:53:40', '2022-04-01 14:53:40'),
+(2, 'Aba South', 2, '2022-04-01 14:53:58', '2022-04-01 14:53:58'),
+(3, 'Arochukwu', 3, '2022-04-01 14:54:26', '2022-04-01 14:54:26'),
+(4, 'Bende', 4, '2022-04-01 14:54:44', '2022-04-01 14:54:44'),
+(5, 'Ikwuano', 5, '2022-04-01 14:55:02', '2022-04-01 14:55:02');
 
 -- --------------------------------------------------------
 
@@ -108,12 +108,12 @@ CREATE TABLE `states` (
 --
 
 INSERT INTO `states` (`id`, `name`, `updated_at`, `created_at`) VALUES
-(1, 'Abia', '2022-04-01 10:57:18', '2022-04-01 10:57:18'),
-(2, 'Adamawa', '2022-04-01 10:57:31', '2022-04-01 10:57:31'),
-(3, 'Akwa Ibom', '2022-04-01 10:57:43', '2022-04-01 10:57:43'),
-(4, 'Anambra', '2022-04-01 10:57:55', '2022-04-01 10:57:55'),
-(5, 'Bauchi', '2022-04-01 10:58:08', '2022-04-01 10:58:08'),
-(6, 'Bayelsa', '2022-04-01 11:19:58', '2022-04-01 11:19:58');
+(1, 'Abia', '2022-04-01 14:57:18', '2022-04-01 14:57:18'),
+(2, 'Adamawa', '2022-04-01 14:57:31', '2022-04-01 14:57:31'),
+(3, 'Akwa Ibom', '2022-04-01 14:57:43', '2022-04-01 14:57:43'),
+(4, 'Anambra', '2022-04-01 14:57:55', '2022-04-01 14:57:55'),
+(5, 'Bauchi', '2022-04-01 14:58:08', '2022-04-01 14:58:08'),
+(6, 'Bayelsa', '2022-04-01 14:19:58', '2022-04-01 14:19:58');
 
 -- --------------------------------------------------------
 
@@ -135,8 +135,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `updated_at`, `created_at`) VALUES
-(1, 'Admin', 'admin@admin.com', '$2y$10$CMBni6UMqwhXhmDxTaysGOR6qQQcSb7AbT6MLYVFFCPXPjEAn/CBC', '2022-03-31 21:47:45', '2022-03-31 21:47:45'),
-(2, 'Admin2', 'admin@admin1.com', '$2y$10$M4FvK5btr2nQrM7Bwn3vCe83itE3B3S0SKoDq16mKHDNsU9XWkAWK', '2022-04-01 00:08:31', '2022-04-01 00:08:31');
+(1, 'Admin', 'admin@admin.com', '$2y$10$CMBni6UMqwhXhmDxTaysGOR6qQQcSb7AbT6MLYVFFCPXPjEAn/CBC', '2022-04-01 13:47:45', '2022-04-01 13:47:45'),
+(2, 'Admin2', 'admin@admin1.com', '$2y$10$M4FvK5btr2nQrM7Bwn3vCe83itE3B3S0SKoDq16mKHDNsU9XWkAWK', '2022-04-01 00:01:31', '2022-04-01 00:01:31'),
+(3, 'Adebis Yinka', 'adebisi@yinka.com', '$2y$10$Au.Rfd6hFIVsaHntobVxN.SYAz68Zj4VBXTCfPMQ6WU/Rrb69Z6x.', '2022-04-01 13:34:19', '2022-04-01 13:34:19'),
+(4, 'Loyalty Solutions', 'loyalty.solutions@tech.com', '$2y$10$XjYCZjPIzIehCMiE4vBmZOGEboCR/XXcdw00AnfVgUj8WsQ1cM/nS', '2022-04-01 13:35:48', '2022-04-01 13:35:48'),
+(5, 'Happy Hours', 'happy.hours@tech.com', '$2y$10$xTC6uArxnh4JRDPwITTLhuJqSjKiR5xr8vJ7C5n3gDv/CCXzthnyu', '2022-04-01 13:36:45', '2022-04-01 13:36:45');
 
 -- --------------------------------------------------------
 
@@ -157,11 +160,11 @@ CREATE TABLE `wards` (
 --
 
 INSERT INTO `wards` (`id`, `name`, `lga_id`, `created_at`, `updated_at`) VALUES
-(3, 'Ward 1', 1, '2022-04-01 10:51:52', '2022-04-01 10:51:52'),
-(4, 'Ward 2', 2, '2022-04-01 10:52:01', '2022-04-01 10:52:01'),
-(5, 'Ward 3', 3, '2022-04-01 10:52:12', '2022-04-01 10:52:12'),
-(6, 'Ward 4', 4, '2022-04-01 10:52:23', '2022-04-01 10:52:23'),
-(7, 'Ward 5', 5, '2022-04-01 10:52:33', '2022-04-01 10:52:33');
+(3, 'Ward 1', 1, '2022-04-01 14:51:52', '2022-04-01 14:51:52'),
+(4, 'Ward 2', 2, '2022-04-01 14:52:01', '2022-04-01 14:52:01'),
+(5, 'Ward 3', 3, '2022-04-01 14:52:12', '2022-04-01 14:52:12'),
+(6, 'Ward 4', 4, '2022-04-01 14:52:23', '2022-04-01 14:52:23'),
+(7, 'Ward 5', 5, '2022-04-01 14:52:33', '2022-04-01 14:52:33');
 
 --
 -- Indexes for dumped tables
@@ -237,7 +240,7 @@ ALTER TABLE `states`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `wards`
